dcfd0bf099e3880c8410c696ddbf31879473b469
rene <linear23150@outlook.com>
2025-12-11 14:40:59 +0800

    change 6th

--- nixos/home.nix
+++ nixos/home.nix
@@ -27,22 +27,23 @@
 		curl
 		fastfetch
 		cmake
-		 ninja
-		  llvmPackages.clang
-            llvmPackages.lldb
-	     clang-tools
-	     bear
-            
-            valgrind
+		ninja
+		llvmPackages.clang  
+        llvmPackages.lldb    
+	    clang-tools 
+	    bear 
+        valgrind    
+        asymptote    
 	    noto-fonts-cjk-sans
-	    	
-	    qt6.qtbase
+	    qt6.qtbase	
+	    
+
 	    (python3.withPackages (p: with p; [
-		  numpy      # 解决 NumPy 未支持的问题
-		  matplotlib # 解决 Matplotlib 未安装的问题
-		  scipy      # (可选，推荐) 科学计算核心库
+		  numpy      
+		  matplotlib 
+		  scipy      
 		  pandas
-		  pyqt6  # (可选) 数据分析库
+		  pyqt6  
 		]))
 	
 	];
