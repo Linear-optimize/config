dca495212664881221f681145d4df8d372fc2b0e
meursault <linear23150@outlook.com>
2025-12-24 10:54:16 +0800

    2025-12-24 commit

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,8 +1,31 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+
 require("config.lazy")
 vim.cmd("colorscheme duskfox")
 
-vim.api.nvim_set_hl(0, "Normal", {
+-- vim.api.nvim_set_hl(0, "Normal", {
+-- bg = "NONE",
+-- ctermbg = "NONE",
+-- })
+
+local transparent = {
   bg = "NONE",
   ctermbg = "NONE",
-})
+}
+
+vim.api.nvim_set_hl(0, "Normal", transparent)
+vim.api.nvim_set_hl(0, "NormalNC", transparent)
+vim.api.nvim_set_hl(0, "EndOfBuffer", transparent)
+
+-- 浮窗 / dap-ui
+vim.api.nvim_set_hl(0, "NormalFloat", transparent)
+vim.api.nvim_set_hl(0, "FloatBorder", transparent)
+
+-- 分割线
+vim.api.nvim_set_hl(0, "WinSeparator", transparent)
+
+-- dap-ui 专用
+vim.api.nvim_set_hl(0, "DapUIVariable", transparent)
+vim.api.nvim_set_hl(0, "DapUIScope", transparent)
+vim.api.nvim_set_hl(0, "DapUIType", transparent)
+vim.api.nvim_set_hl(0, "DapUIValue", transparent)
+vim.api.nvim_set_hl(0, "DapUIFrameName", transparent)
