4239d324b35f35b6d9a4629fa80743e3dc9f7b5e
rene <linear23150@outlook>
2026-02-22 09:58:26 +0800

    add docker-compose.yml

--- Docker_Config/GoLang/build.sh
+++ Docker_Config/GoLang/build.sh
@@ -2,7 +2,6 @@
 set -e
 set -x
 
-
 go env -w GO111MODULE=on
 go env -w GOPROXY=https://goproxy.cn,direct
 
@@ -10,13 +9,9 @@ if [ -f /etc/apt/sources.list.d/debian.sources ]; then
     sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list.d/debian.sources
 fi
 
-
 apt-get update
 
 apt-get install -y --no-install-recommends gcc libc6-dev make git
-
-
-
 
 apt-get clean
 rm -rf /var/lib/apt/lists/*
